72205fec2598ce3da3cff5524439bfa024650392
Dougwar <dougrezon@hotmail.com>
2020-08-31 11:51:05 -0300

    Add files via upload

---
+++ Lua_Scripts/I+1 Sentences.lua
@@ -0,0 +1,53 @@
+--search and find sentences with one unknow word
+--By Dougwar
+
+local cta = require 'cta'
+local lfs = require 'lfs'
+
+w = 0
+
+local function findSentencesInDocument( filename, known, seen )
+    local document = cta.Document( filename )
+    for line in document:lines() do
+        for sentence in line:sentences() do
+        for word in sentence:words() do
+		    if not known:contains( word ) then
+                w = w + 1
+             end
+				
+			end
+			if 1 == w  then
+				print ( sentence )	
+        end
+		w = 0	
+    end
+end
+ end
+
+
+local function traverseDirectory( directory, known, seen )
+    for file in lfs.dir( directory ) do
+        if file ~= "." and file ~= ".." then
+
+            local fullPath = directory .. '/' .. file
+            local mode = lfs.attributes( fullPath, 'mode' )
+
+            if mode == "directory" then
+                traverseDirectory( fullPath )
+            elseif mode == "file" then
+                if file:match( "%.txt$" ) then
+                    findSentencesInDocument( fullPath, known, seen )
+                end
+            end
+        end
+    end
+end
+
+local known = cta.knownWords()
+local seen = {}
+
+local directory = cta.askUserForDirectory()
+
+if directory ~= nil then
+    traverseDirectory( directory, known, seen )
+end
